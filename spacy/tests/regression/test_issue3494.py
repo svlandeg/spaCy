549ab8a6296162d6fba23c5aa74d5930207a3c5b
svlandeg <sofie.vanlandeghem@gmail.com>
2019-07-11 17:52:16 +0200

    fixing the issue by reading nlp from file instead of reinitializing

--- spacy/tests/regression/test_issue3494.py
+++ spacy/tests/regression/test_issue3494.py
@@ -1,6 +1,7 @@
 # coding: utf8
 from __future__ import unicode_literals
 
+import spacy
 from spacy.matcher import PhraseMatcher
 from spacy.lang.en import English
 from spacy.compat import pickle
@@ -12,16 +13,17 @@ def test_issue3494(en_vocab):
     matcher.add("TEST1", None, nlp1("a"), nlp1("b"), nlp1("c"))
     matcher.add("TEST2", None, nlp1("d"))
 
-    data = pickle.dump(matcher, open("matcher.pickle", "wb"))
+    pickle.dump(matcher, open("matcher.pickle", "wb"))
 
     pickled_matcher = pickle.load(open("matcher.pickle", "rb"))
-
     print()
     m = matcher(nlp1("a b c"))
     m_pickeld = pickled_matcher(nlp1("a b c"))
     assert m == m_pickeld
 
-    nlp2 = English()
+    nlp1.to_disk("mynlp")
+    nlp2 = spacy.load("mynlp")
+
     loaded_matcher = pickle.load(open("matcher.pickle", "rb"))
     m_loaded = loaded_matcher(nlp2("a b c"))
     assert m == m_loaded
